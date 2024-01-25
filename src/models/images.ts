edfffb067ea1fd7a7950592f2d448146c8f7140e
Morel-D <morelDen@gmail.com>
2024-01-25 06:39:25 +0100

    zod data schema

---
+++ src/models/images.ts
@@ -0,0 +1,29 @@
+import { z } from "zod"
+
+const BascisImageSchema = z.object({
+    page: z.number(),
+    per_page: z.number(),
+    prev_page: z.string().optional(),
+    next_page: z.string().optional(),
+    total_results: z.number() 
+})
+
+
+const PhotoShema = z.object({
+    id: z.number(),
+    width: z.number(),
+    height: z.number(),
+    url: z.string(),
+    src: z.object({
+        large: z.string()
+    }),
+    alt: z.string(),
+    blurredDataUrl: z.string().optional(),
+})
+
+export const ImageSchemaWithPhotos = BascisImageSchema.extend({
+    photo: z.array(PhotoShema)
+})
+
+export type Photo = z.infer<typeof PhotoShema>
+export type ImageResults = z.infer<typeof ImageSchemaWithPhotos>
